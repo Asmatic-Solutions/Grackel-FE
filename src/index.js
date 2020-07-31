2b6ce67b2800e723ee24eff27b8b1404177cf7e5
MrR3set <bairon.pazrobles@gmail.com>
2020-07-31 14:53:15 -0700

    Fixed typo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-import {Provide} from "react";
+import {Provider} from "react-redux";
 import {createStore,applyMiddleware} from "redux"
 import thunk from "redux-thunk"
 import Reducers from "./components/Redux/reducers/combineReducers"
@@ -11,11 +10,11 @@ import Reducers from "./components/Redux/reducers/combineReducers"
 const store = createStore(Reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <Provide store={store}>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </Provide>,
+    </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
