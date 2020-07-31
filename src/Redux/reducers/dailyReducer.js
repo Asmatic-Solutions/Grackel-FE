2b47a035ae09a18c031a18c6a92987269263fd1f
MrR3set <bairon.pazrobles@gmail.com>
2020-07-31 13:34:27 -0700

    Daily reducer cases set up

--- src/Redux/reducers/dailyReducer.js
+++ src/Redux/reducers/dailyReducer.js
@@ -1,21 +1,34 @@
 import {
-    ADD_D,
-    GET_D
+    FECTH_DAILY_START,
+    FECTH_DAILY_SUCCESS,
+    FECTH_DAILY_FAILURE,
+    ADD_DAILY_START,
+    ADD_DAILY_SUCCESS,
+    ADD_DAILY_FAILURE
 } from "../actions/dailyActions"
+import { act } from "react-dom/test-utils";
 
 const initialState = {
-
+    User_ID:"",
+    Date:"",
+    DailyCount:"",
+    Success:true,
 }
 
 export const dailyReducer = (state=initialState,action)=>{
-
+    console.log("---",action.type,action.payload)
     switch(action.type){
-        case ADD_D:
-            return{
-                ...state,
-            };
+        case FECTH_DAILY_START:
+            return{...state,};
+        case FECTH_DAILY_SUCCESS:
+           return{...state}
+        case FECTH_DAILY_FAILURE:
+            return{...state,}
+        case ADD_DAILY_START:
+            return{...state,};
+        case ADD_DAILY_SUCCESS:
+            return{...state}
+        case ADD_DAILY_FAILURE:
+            return{...state,}
     }
-
-
-
 }
