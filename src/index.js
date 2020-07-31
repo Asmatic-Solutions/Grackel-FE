c90056b801a703222475a34a2291f09e981fa67f
MrR3set <bairon.pazrobles@gmail.com>
2020-07-31 13:46:51 -0700

    Set up store and middleware

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {Provide} from "react";
+import {createStore,applyMiddleware} from "redux"
+import thunk from "redux-thunk"
+import Reducers from "./components/Redux/reducers/combineReducers"
+
+const store = createStore(Reducers, applyMiddleware(thunk))
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provide store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provide>,
   document.getElementById('root')
 );
 
