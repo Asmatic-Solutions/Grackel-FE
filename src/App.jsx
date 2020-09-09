532676bd0b8c1bc477f31b92e884a771398ee931
MrR3set <bairon.pazrobles@gmail.com>
2020-09-08 20:17:09 -0700

    Set up header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import LoginPage from "./components/Auth/Login/LoginPage"
+import AddPage from "./components/Homepage/AddPage"
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage></LoginPage>
+      <div className="header">Grackel</div>
+      <AddPage></AddPage>
+      {/* <LoginPage></LoginPage> */}
+      
     </div>
   );
 }
