090e61a16542b4f26f253e9ceca1a9a625f66012
MrR3set <bairon.pazrobles@gmail.com>
2020-09-10 12:55:17 -0700

    Changed addKcal function

--- src/components/Homepage/ManuallyAdd.jsx
+++ src/components/Homepage/ManuallyAdd.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-function ManuallyAdd() {
-  return (
-    <div className="add-meals-page">
-        <div className="option add-manually">
-            <h1 className="title">Add manually</h1>
-            <div className="controls">
-                <input type="number" min="1" max="5000" name="amount" placeholder="Enter amount"/>
-                <button>Add</button>
+function ManuallyAdd(props) {
+
+    return (
+        <div className="add-meals-page">
+            <div className="option add-manually">
+                <h1 className="title">Add manually</h1>
+                <div className="controls">
+                    <input type="number" min="1" max={5000} name="amount" placeholder="Enter amount"  onChange={props.handleKcalChange}/>
+                    <button onClick={props.addKcalAmount}>Add</button>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default ManuallyAdd;
