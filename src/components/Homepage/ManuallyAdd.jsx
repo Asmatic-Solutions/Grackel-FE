577dea12ca24a8c340fd251aa8e1c794e8e46dfe
MrR3set <bairon.pazrobles@gmail.com>
2020-09-10 13:59:18 -0700

    Updated class names for manually add

--- src/components/Homepage/ManuallyAdd.jsx
+++ src/components/Homepage/ManuallyAdd.jsx
@@ -1,17 +1,15 @@
-import React,{useState} from 'react';
+import React from 'react';
 
 function ManuallyAdd(props) {
 
     return (
-        <div className="add-meals-page">
-            <div className="option add-manually">
-                <h1 className="title">Add manually</h1>
-                <div className="controls">
-                    <input type="number" min="1" max={5000} name="amount" placeholder="Enter amount"  onChange={props.handleKcalChange}/>
-                    <button onClick={props.addKcalAmount}>Add</button>
-                </div>
-            </div>
+    <div className="option wrapper add-manually">
+        <h1 className="title">Add manually</h1>
+        <div className="controls">
+            <input type="number" min="1" max={5000} name="amount" placeholder="Enter amount"  onChange={props.handleKcalChange}/>
+            <button onClick={props.addKcalAmount}>Add</button>
         </div>
+    </div>
     );
 }
 
