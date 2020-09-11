0b0899f43b7a94441842f55a2ab337289a271da7
MrR3set <bairon.pazrobles@gmail.com>
2020-09-11 13:00:42 -0700

    Redirects to home uppon login

--- src/components/Auth/Login/LoginPage.jsx
+++ src/components/Auth/Login/LoginPage.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react"
+import { Redirect, useHistory } from "react-router-dom"
 import {axiosWithAuth} from "../../Utils/axiosWithAuth"
 import LoginForm from "./LoginForm"
 
 function LoginPage(){
     const [creedentials,setCreedentials] = useState({username:"",password:""})
-    // const [creedentials,setCreedentials] = useState({username:"",password:"",email:""})
+    const history = useHistory()
 
     const handleSubmit=(e)=>{
         e.preventDefault()
         axiosWithAuth().post("auth/login",creedentials).then(res=>{
             localStorage.setItem("token",res.data.token)
+            history.push("/")
         }).catch(err=>{
             console.log(err)
         })
@@ -21,7 +23,9 @@ function LoginPage(){
     return(
         <div>
             <h1>Login test</h1>
-            <LoginForm creedentials={creedentials} handleChange={handleChange} handleSubmit={handleSubmit}/>
+            {localStorage.getItem("token")
+            ?<Redirect to="/"/>
+            :<LoginForm creedentials={creedentials} handleChange={handleChange} handleSubmit={handleSubmit}/>}
         </div>
     )
 }
