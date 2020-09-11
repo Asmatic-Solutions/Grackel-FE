96f6a2c40b0d76a65441a6c5d648fab005cce76b
MrR3set <bairon.pazrobles@gmail.com>
2020-09-11 12:59:39 -0700

    Set up router

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import LoginPage from "./components/Auth/Login/LoginPage"
 import AddPage from "./components/Homepage/AddPage"
 import './App.scss';
+import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/Utils/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
       <div className="header">Grackel</div>
-      <AddPage></AddPage>
-      {/* <LoginPage></LoginPage> */}
-      
+      <Switch>
+        <Route path="/login" component={LoginPage}/>
+        <PrivateRoute exact path="/" component={AddPage}/>
+      </Switch>
     </div>
   );
 }
