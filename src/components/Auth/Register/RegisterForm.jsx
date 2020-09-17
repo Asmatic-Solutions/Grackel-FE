f7d2e7f939b735fbf05c55c87d152003c7bdef2f
MrR3set <bairon.pazrobles@gmail.com>
2020-09-16 21:20:25 -0700

    Refactored register form

--- src/components/Auth/Register/RegisterForm.jsx
+++ src/components/Auth/Register/RegisterForm.jsx
@@ -2,42 +2,40 @@ import React from "react"
 function LoginForm(props){
 
     return(
-        <div className="form-wrapper">
-            <form className="login-form">
-                <label>
-                    Username 
-                    <input 
-                        value={props.creedentials.username}
-                        onChange={props.handleChange}
-                        name="username"
-                        placeholder="Username"
-                        type="text"
-                    />
-                </label>
-                <label>
-                    Password 
-                    <input 
-                        value={props.creedentials.password}
-                        onChange={props.handleChange}
-                        name="password"
-                        placeholder="Password"
-                        type="text"
-                    />
-                </label>
-                <label>
-                    Email 
-                    <input 
-                        value={props.creedentials.email}
-                        onChange={props.handleChange}
-                        name="email"
-                        placeholder="example@example.com"
-                        type="email"
-                    />
-                </label>
+        <form className="login-form form-wrapper">
+            <label>
+                Username 
+                <input 
+                    value={props.creedentials.username}
+                    onChange={props.handleChange}
+                    name="username"
+                    placeholder="Username"
+                    type="text"
+                />
+            </label>
+            <label>
+                Password 
+                <input 
+                    value={props.creedentials.password}
+                    onChange={props.handleChange}
+                    name="password"
+                    placeholder="Password"
+                    type="text"
+                />
+            </label>
+            <label>
+                Email 
+                <input 
+                    value={props.creedentials.email}
+                    onChange={props.handleChange}
+                    name="email"
+                    placeholder="example@example.com"
+                    type="email"
+                />
+            </label>
 
-                <button onClick={props.handleSubmit} name="Sign up">Sign up</button>
-            </form>
-        </div>
+            <button onClick={props.handleSubmit} name="Sign up">Sign up</button>
+        </form>
     )
 }
 
