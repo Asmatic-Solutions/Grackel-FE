bc463f71abc4a039b59a8505bf157d4413fdb788
MrR3set <bairon.pazrobles@gmail.com>
2020-09-16 21:20:18 -0700

    Refactored login form

--- src/components/Auth/Login/LoginForm.jsx
+++ src/components/Auth/Login/LoginForm.jsx
@@ -2,31 +2,29 @@ import React from "react"
 function LoginForm(props){
 
     return(
-        <div className="form-wrapper">
-            <form className="login-form">
-                <label>
-                    Username 
-                    <input 
-                        value={props.creedentials.username}
-                        onChange={props.handleChange}
-                        name="username"
-                        placeholder="Username"
-                        type="text"
-                    />
-                </label>
-                <label>
-                    Password 
-                    <input 
-                        value={props.creedentials.password}
-                        onChange={props.handleChange}
-                        name="password"
-                        placeholder="Password"
-                        type="text"
-                    />
-                </label>
-                <button onClick={props.handleSubmit} name="login">Login</button>
-            </form>
-        </div>
+        <form className="login-form form-wrapper">
+            <label>
+                Username 
+                <input 
+                    value={props.creedentials.username}
+                    onChange={props.handleChange}
+                    name="username"
+                    placeholder="Username"
+                    type="text"
+                />
+            </label>
+            <label>
+                Password 
+                <input 
+                    value={props.creedentials.password}
+                    onChange={props.handleChange}
+                    name="password"
+                    placeholder="Password"
+                    type="text"
+                />
+            </label>
+            <button onClick={props.handleSubmit} name="login">Login</button>
+        </form>
     )
 }
 
