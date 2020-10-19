093c90aef0522ec913e73b8a48c832f0f05f9d2a
MrR3set <bairon.pazrobles@gmail.com>
2020-10-18 18:43:38 -0700

    Redirect to login page when api returns 401

--- src/components/Redux/reducers/dailyReducer.js
+++ src/components/Redux/reducers/dailyReducer.js
@@ -21,14 +21,26 @@ export const dailyReducer = (state=initialState,action)=>{
         case FETCH_DAILY_SUCCESS:
            return{...action.payload}
         case FETCH_DAILY_FAILURE:
+            checkToken(action.payload.response.status)
             return{...state,}
         case ADD_DAILY_START:
             return{...state,};
         case ADD_DAILY_SUCCESS:
             return{...action.payload}
         case ADD_DAILY_FAILURE:
+
+            checkToken(action.payload.response.status)
+            console.log("--",{state,action})
             return{...state,}
         default:
             return state
+    }
+}
+
+function checkToken(statusCode){
+    // Check if error code is 401
+    if(statusCode === 401){
+        window.localStorage.removeItem("token")
+        window.history.go("/login")
     }
 }
