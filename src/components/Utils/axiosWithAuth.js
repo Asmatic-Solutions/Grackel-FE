61a3a7d35a3d970216d8b4df616ede54f17b2f20
MrR3set <bairon.pazrobles@gmail.com>
2020-10-27 22:07:21 -0700

    Removed useless spaces

--- src/components/Utils/axiosWithAuth.js
+++ src/components/Utils/axiosWithAuth.js
@@ -2,14 +2,10 @@ import axios from "axios"
 
 export const axiosWithAuth = () => {
     const token = window.localStorage.getItem("token")
-
     return axios.create({
         baseURL:"https://grackel-testing.herokuapp.com/api",
         headers:{
             authorization:token
         }
     })
-
-
-
 }
