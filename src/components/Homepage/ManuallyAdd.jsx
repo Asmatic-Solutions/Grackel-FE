26ad9d67f4e122106cc70d8f59060f8a2ec88e21
MrR3set <bairon.pazrobles@gmail.com>
2020-11-07 21:46:52 -0800

    Changed title

--- src/components/Homepage/ManuallyAdd.jsx
+++ src/components/Homepage/ManuallyAdd.jsx
@@ -4,7 +4,7 @@ function ManuallyAdd(props) {
 
     return (
     <div className="option wrapper add-manually">
-        <h1 className="title">Add manually</h1>
+        <h1 className="title">Add kcals manually</h1>
         <div className="controls">
             <input type="number" min="1" max={5000} name="amount" placeholder="Enter amount"  onChange={props.handleKcalChange}/>
             <button onClick={props.addKcalAmount}>Add</button>
