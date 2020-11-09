e5f6aa06bad0380b6ecb49dfa274664c50259983
MrR3set <bairon.pazrobles@gmail.com>
2020-11-08 20:07:24 -0800

    Created new component for adding manual meals

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import LoginPage from "./components/Auth/Login/LoginPage"
-import RegisterPage from "./components/Auth/Register/RegisterPage"
-import HomePage from "./components/Homepage/HomePage"
+import LoginPage from "./components/Auth/Login/LoginPage";
+import RegisterPage from "./components/Auth/Register/RegisterPage";
+import HomePage from "./components/Homepage/HomePage";
+import AddMealManually from "./components/AddMealsPage/AddMealManually"
 import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/Utils/PrivateRoute';
@@ -13,7 +14,7 @@ function App() {
       <Switch>
         <Route path="/login" component={LoginPage}/>
         <Route path="/register" component={RegisterPage}/>
-
+        <PrivateRoute path="/meal/manual" component={AddMealManually}/>
 
         <PrivateRoute exact path="/" component={HomePage}/>
       </Switch>
---
+++ src/components/AddMealsPage/AddMealManually.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./AddMealManually.scss";
+
+function AddMealManuallyPage(props) {
+
+    return (
+        <div className="add--meal-manually wrapper">
+            <h1>Add meal manually</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus beatae quo aliquam tempora rem veniam odit voluptatibus! Qui rem velit, doloremque eveniet optio impedit voluptas et porro quos recusandae omnis necessitatibus quas unde excepturi totam praesentium autem fugit, dolor eos molestias commodi reprehenderit. Expedita mollitia recusandae hic explicabo quidem. Consequuntur?</p>
+        </div>
+    );
+}
+
+export default AddMealManuallyPage;
