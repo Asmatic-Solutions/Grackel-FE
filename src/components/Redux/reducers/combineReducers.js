db752095e2abfd00e080cce38c1402805e462693
MrR3set <bairon.pazrobles@gmail.com>
2020-12-08 19:39:27 -0800

    combined reducers

--- src/components/Redux/reducers/combineReducers.js
+++ src/components/Redux/reducers/combineReducers.js
@@ -1,8 +1,10 @@
 import {combineReducers } from "redux"
 import {dailyReducer} from "./dailyReducer"
 import {goalReducer} from "./goalReducer"
+import {mealReducer} from "./mealReducer"
 
 export default combineReducers({
     dailyReducer,
-    goalReducer
+    goalReducer,
+    mealReducer
 })
