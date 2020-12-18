605a2acbf1c25cd4dbb629e14d9fb5299b28d445
MrR3set <bairon.pazrobles@gmail.com>
2020-12-17 22:55:54 -0800

    Class size from props

--- src/components/Homepage/HomePageLink.jsx
+++ src/components/Homepage/HomePageLink.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function HomePageLink({title,path,info}) {
+function HomePageLink({title,path,info,size}) {
 
     return (
-        <Link to={path} className="option wrapper link">
+        <Link to={path} className={`option wrapper link ${size}`}>
             <h1 className="title">{title}</h1>
             {info?<p>{info}</p>:null}
         </Link>
